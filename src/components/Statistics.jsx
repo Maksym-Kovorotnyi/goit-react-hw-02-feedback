3bf7574eb144cd318d40c7c0f91de3a4b9be7dc4
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-02-18 16:13:52 +0200

    Final

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css'
+export const Statistics = ({ good, neutral, bad, total, positivePercentag }) => (
+    <>
+        <p className={`${css.text} ${css.good}`}>Good: {good}</p>
+        <p className={`${css.text} ${css.neutral}`}>Neutral: {neutral}</p>
+        <p className={`${css.text} ${css.bad}`}>Bad: {bad}</p>
+        <p className={css.text}>Total: {total}</p>
+        {total ? <p className={css.text}>Positive feedback: {positivePercentag} %</p> : <p className={css.text}>Positive feedback: 0 %</p>} 
+</>
+)
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.func.isRequired,
+  positivePercentag: PropTypes.string.isRequired,
+};
